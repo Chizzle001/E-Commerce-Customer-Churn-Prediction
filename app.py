bfd299140e9da6e2133495a66cf93f134ed21f6a
Chizzle_001 <chirag1346@gmail.com>
2023-09-07 13:32:38 +0530

    Data Ingestion Phase Completed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion,DataIngestionConfig
 import sys
 
 if __name__ == '__main__':
@@ -8,8 +9,10 @@ if __name__ == '__main__':
     
     try:
         
-        a = 1/0 
-        
+       # Data_Ingestion_config = DataIngestionConfig()
+       Data_Ingestion = DataIngestion()
+       Data_Ingestion.Initiate_Data_Ingestion()
+       
     except Exception as e:
         logging.info('Custom Exception')
         raise CustomException(e,sys)
